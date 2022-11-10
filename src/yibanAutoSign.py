a87b674edee174480ce9c65e58d2b02aa0f937aa
Qs315490 <qs315490@qq.com>
2022-11-10 14:08:48 +0800

    test

--- src/yibanAutoSign.py
+++ src/yibanAutoSign.py
@@ -4,15 +4,15 @@ import random
 import re
 import threading
 import time
-from base64 import b64encode
+# from base64 import b64encode
 
 from serverChan import ServerChan
 from userData import user_data
 
 try:
     import requests
-    from Crypto.Cipher import PKCS1_v1_5
-    from Crypto.PublicKey import RSA
+    # from Crypto.Cipher import PKCS1_v1_5
+    # from Crypto.PublicKey import RSA
     from requests.exceptions import RequestException
 except ModuleNotFoundError:
     print("缺少依赖! 请安装依赖！")
@@ -34,42 +34,47 @@ def get_csrf_token():
     return csrf_token
 
 
-def encrypt_rsa(data: str) -> str:
-    """
-    登录密码加密
-    :param data: 待加密的密码
-    :return: str
-    """
-    rsa_key = '''-----BEGIN PUBLIC KEY-----
-    MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
-    Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
-    XC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx+WasvbS0HuYpF8+
-    KPl7LJPjtI4XAAOLBntQGnPwCX2Ff/LgwqkZbOrHHkN444iLmViCXxNUDUMUR9bP
-    A9/I5kwfyZ/mM5m8+IPhSXZ0f2uw1WLov1P4aeKkaaKCf5eL3n7/2vgq7kw2qSmR
-    AGBZzW45PsjOEvygXFOy2n7AXL9nHogDiMdbe4aY2VT70sl0ccc4uvVOvVBMinOp
-    d2rEpX0/8YE0dRXxukrM7i+r6lWy1lSKbP+0tQxQHNa/Cjg5W3uU+W9YmNUFc1w/
-    7QT4SZrnRBEo++Xf9D3YNaOCFZXhy63IpY4eTQCJFQcXdnRbTXEdC3CtWNd7SV/h
-    mfJYekb3GEV+10xLOvpe/+tCTeCDpFDJP6UuzLXBBADL2oV3D56hYlOlscjBokNU
-    AYYlWgfwA91NjDsWW9mwapm/eLs4FNyH0JcMFTWH9dnl8B7PCUra/Lg/IVv6HkFE
-    uCL7hVXGMbw2BZuCIC2VG1ZQ6QD64X8g5zL+HDsusQDbEJV2ZtojalTIjpxMksbR
-    ZRsH+P3+NNOZOEwUdjJUAx8CAwEAAQ==
-    -----END PUBLIC KEY-----'''
-    data_byte = bytes(data, 'utf-8')
-    encrypt_obj = PKCS1_v1_5.new(RSA.importKey(rsa_key))
-    data_encrypt = b64encode(encrypt_obj.encrypt(data_byte))
-    return data_encrypt.decode('utf-8')
+# def encrypt_rsa(data: str) -> str:
+#     """
+#     登录密码加密
+#     :param data: 待加密的密码
+#     :return: str
+#     """
+#     rsa_key = '''-----BEGIN PUBLIC KEY-----
+#     MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEA6aTDM8BhCS8O0wlx2KzA
+#     Ajffez4G4A/QSnn1ZDuvLRbKBHm0vVBtBhD03QUnnHXvqigsOOwr4onUeNljegIC
+#     XC9h5exLFidQVB58MBjItMA81YVlZKBY9zth1neHeRTWlFTCx+WasvbS0HuYpF8+
+#     KPl7LJPjtI4XAAOLBntQGnPwCX2Ff/LgwqkZbOrHHkN444iLmViCXxNUDUMUR9bP
+#     A9/I5kwfyZ/mM5m8+IPhSXZ0f2uw1WLov1P4aeKkaaKCf5eL3n7/2vgq7kw2qSmR
+#     AGBZzW45PsjOEvygXFOy2n7AXL9nHogDiMdbe4aY2VT70sl0ccc4uvVOvVBMinOp
+#     d2rEpX0/8YE0dRXxukrM7i+r6lWy1lSKbP+0tQxQHNa/Cjg5W3uU+W9YmNUFc1w/
+#     7QT4SZrnRBEo++Xf9D3YNaOCFZXhy63IpY4eTQCJFQcXdnRbTXEdC3CtWNd7SV/h
+#     mfJYekb3GEV+10xLOvpe/+tCTeCDpFDJP6UuzLXBBADL2oV3D56hYlOlscjBokNU
+#     AYYlWgfwA91NjDsWW9mwapm/eLs4FNyH0JcMFTWH9dnl8B7PCUra/Lg/IVv6HkFE
+#     uCL7hVXGMbw2BZuCIC2VG1ZQ6QD64X8g5zL+HDsusQDbEJV2ZtojalTIjpxMksbR
+#     ZRsH+P3+NNOZOEwUdjJUAx8CAwEAAQ==
+#     -----END PUBLIC KEY-----'''
+#     data_byte = bytes(data, 'utf-8')
+#     encrypt_obj = PKCS1_v1_5.new(RSA.importKey(rsa_key))
+#     data_encrypt = b64encode(encrypt_obj.encrypt(data_byte))
+#     return data_encrypt.decode('utf-8')
 
 
 class YiBan:
 
-    def __init__(self, phone, passwd, address,
-                 server_chan: ServerChan) -> None:
-        self.__phone = phone
-        self.__password = passwd
+    def __init__(self,
+                 phone,
+                 passwd,
+                 address,
+                 server_chan: ServerChan,
+                 photo: str = '') -> None:
+        self.phone = phone
+        self.password = passwd
         self.CSRF = get_csrf_token()  # '64b5c616dc98779ee59733e63de00dd5'
         self.address = address
         self.session = requests.session()
         self.server_chan = server_chan
+        self.photo = photo
 
     def req(self,
             url,
@@ -96,6 +101,8 @@ class YiBan:
             reqs = self.session.get
         elif method == 'post':
             reqs = self.session.post
+        elif method == 'put':
+            reqs = self.session.put
         else:
             self.session.close()  # close session
             raise Exception('Requests method error.')
@@ -117,7 +124,7 @@ class YiBan:
         raise Exception('连接异常且重试后失败')
 
     def do_login(self) -> bool:
-        push_data = {'account': self.__phone, 'password': self.__password}
+        push_data = {'account': self.phone, 'password': self.password}
         # 这个请求没有返回值
         self.req('https://www.yiban.cn/login/dologinAjax',
                  'post',
@@ -129,7 +136,7 @@ class YiBan:
         try:
             self.session.cookies.get_dict()['yiban_user_token']
         except KeyError:
-            self.server_chan.log(f'{self.__phone} 登录失败')
+            self.server_chan.log(f'{self.phone} 登录失败')
             return False
         return True
 
@@ -150,7 +157,7 @@ class YiBan:
         try:
             back_json['data']['PersonName']
         except KeyError:
-            msg = f'{self.__phone} 获取授权失败, 可能未授权校本化.'
+            msg = f'{self.phone} 获取授权失败, 可能未授权校本化.'
             self.server_chan.log(msg)
             return {'code': 1, 'msg': msg}
         return back_json
@@ -162,6 +169,10 @@ class YiBan:
         """
 
         def get_status() -> dict:
+            # 获取照片要求
+            self.req(
+                f'https://api.uyiban.com/nightAttendance/student/index/photoRequirements?CSRF={self.CSRF}'
+            )
             # 设备状态?
             self.req(
                 f'https://api.uyiban.com/nightAttendance/student/index/deviceState?CSRF={self.CSRF}'
@@ -187,8 +198,37 @@ class YiBan:
         get_status()  # 不加会按之前获取状态的时间, 从而导致失败
 
         if time_now < time_range['EndTime'] or DEBUG:
+            photo_path = ''
+            if self.photo != '':
+                with open(self.photo, 'rb') as file:
+                    photo_raw = file.read()
+
+                photo_size = os.path.getsize(self.photo)
+                # 上传照片
+                back = self.req(
+                    "https://api.uyiban.com/nightAttendance/student/index/uploadUri",
+                    params={
+                        "name": "image.jpg",
+                        "type": "image/jpeg",
+                        "size": str(photo_size),  #"99819", # 图片大小
+                        "CSRF": self.CSRF
+                    })
+                try:
+                    back_url = back['data']['signedUrl']
+                    back = self.req(back_url,
+                                    'put',
+                                    data=photo_raw,
+                                    headers={"Content-Type": "image/jpeg"})
+                    print(self.phone, "照片上传返回", back)
+                    photo_path = re.findall("night[\w/]*.jpg", back_url)[0]
+                except KeyError:
+                    msg = f'{self.phone} 照片上传失败'
+                    self.server_chan.log(msg)
+                    return msg
+
             push_data = {
-                "AttachmentFileName": "nightattendance/20221109/f574dd9907994825b40086e81d4f88ad.jpg",
+                "AttachmentFileName":
+                photo_path,  # "nightattendance/20221109/f574dd9907994825b40086e81d4f88ad.jpg",
                 "Code": "",
                 "PhoneModel": "",
                 "SignInfo": self.address,
@@ -200,19 +240,19 @@ class YiBan:
                     'post',
                     data=push_data).json()
             except RequestException:
-                msg = f'{self.__phone} 网络或参数异常, 签到失败'
+                msg = f'{self.phone} 网络或参数异常, 签到失败'
                 self.server_chan.log(msg)
                 return msg
 
             if back['code'] == 0 and back['data'] is True:
-                msg = f'{self.__phone} 签到成功'
+                msg = f'{self.phone} 签到成功'
             else:
-                msg = f'{self.__phone} 签到失败: {back["msg"]}'
+                msg = f'{self.phone} 签到失败: {back["msg"]}'
                 # print(back)
             self.server_chan.log(msg)
             return msg
 
-        msg = f'{self.__phone} 签到失败未到签到时间'
+        msg = f'{self.phone} 签到失败未到签到时间'
         self.server_chan.log(msg)
         return msg
 
